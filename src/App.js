4b6e8c5125f0873667318d5808fdac607d85b04c
AdiFischer <adifischer1@gmail.com>
2022-10-27 14:42:29 -0400

    added state and last

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import { useState } from 'react'
 import logo from './logo.svg';
-import './App.css';
+import './App.css';//. in same foler as crs
+const names = ['adi', 'jack', 'mido', 'eli', 'ben', 'jenny', 'alon', 'idan', 'eddi', 'noam', 'sharon', 'todd']
 
 function App() {
+  const [myName, setMyName] = useState ('Adi')
+  const [counter, setCounter] = useState (0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {myName} is super cool
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {names.map(name => (
+        <button key={name} onClick={() => setMyName(name)}>{name}</button>
+       ))}
+        <button onClick={() => setMyName('Adi')}>Adi</button>
+        <p>You clicked the button {counter} times.</p>
+        <button onClick={() => setCounter(counter - 1)}>-1</button>
+        <button onClick={() => setCounter(0)}>reset</button>
+        <button onClick={() => setCounter(counter + 1)}>+1</button>
       </header>
     </div>
   );
